df20768eded7f69412fb9b16375fd14cb8df1c6b
LasseVKP <74265702+LasseVKP@users.noreply.github.com>
2023-10-12 21:33:54 +0200

    Initial fabric mod

---
+++ src/client/java/lassevkp/revivals/RevivalsClient.java
@@ -0,0 +1,10 @@
+package lassevkp.revivals;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RevivalsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/lassevkp/revivals/RevivalsDataGenerator.java
@@ -0,0 +1,11 @@
+package lassevkp.revivals;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RevivalsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
