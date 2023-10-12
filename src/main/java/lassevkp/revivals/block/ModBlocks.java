1fa6e6318a249d155a2e164718283d601899962a
LasseVKP <74265702+LasseVKP@users.noreply.github.com>
2023-10-12 23:02:46 +0200

    Add base for Items and Blocks

---
+++ src/main/java/lassevkp/revivals/block/ModBlocks.java
@@ -0,0 +1,35 @@
+package lassevkp.revivals.block;
+
+import lassevkp.revivals.Revivals;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+
+    public static final Block COOL_BLOCK = registerBlock("cool_block",
+            new Block(FabricBlockSettings.copyOf(Blocks.STONE).sounds(BlockSoundGroup.ANCIENT_DEBRIS)));
+
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, new Identifier(Revivals.MOD_ID, name), block);
+    }
+
+    private static Item registerBlockItem(String name, Block block){
+        return Registry.register(Registries.ITEM, new Identifier(Revivals.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings()));
+    }
+
+    public static void registerModBlocks() {
+        Revivals.LOGGER.info("Registering blocks for " + Revivals.MOD_ID);
+    }
+
+
+}
---
+++ src/main/java/lassevkp/revivals/item/ModItems.java
@@ -0,0 +1,43 @@
+package lassevkp.revivals.item;
+
+import lassevkp.revivals.Revivals;
+import lassevkp.revivals.block.ModBlocks;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+
+    public static final Item RESURRECTION_TOTEM = registerItem("resurrection_totem", new Item(new FabricItemSettings()));
+
+    public static final ItemGroup MOD_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(Revivals.MOD_ID, "revivals"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.revivals"))
+                    .icon(() -> new ItemStack(ModItems.RESURRECTION_TOTEM))
+                    .entries((displayContext, entries) -> {
+                        entries.add(RESURRECTION_TOTEM);
+                        entries.add(ModBlocks.COOL_BLOCK);
+                    }).build());
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(Revivals.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        Revivals.LOGGER.info("Registering items for " + Revivals.MOD_ID);
+    }
+
+
+
+}
