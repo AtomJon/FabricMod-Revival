2540a5bc1423e41a6113d5dfb4d69039372a39e1
LasseVKP <74265702+LasseVKP@users.noreply.github.com>
2023-10-14 16:21:22 +0200

    Revert "Push to other computer"
    This reverts commit ce2cceec5c07a85d2ed27265b06e5e11a2a9ee8b.

--- src/main/java/lassevkp/revivals/Revivals.java
+++ src/main/java/lassevkp/revivals/Revivals.java
@@ -4,6 +4,11 @@ import lassevkp.revivals.block.ModBlocks;
 import lassevkp.revivals.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
