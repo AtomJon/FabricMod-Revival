f1ed6ba94c935698b3e0c7bb7ddf05e2c848a0b2
LasseVKP <Lassevkp@gmail.com>
2023-11-13 11:51:53 +0100

    Fix screen alignments

--- src/client/java/lassevkp/revivals/screen/RitualTableScreen.java
+++ src/client/java/lassevkp/revivals/screen/RitualTableScreen.java
@@ -45,8 +45,8 @@ public class RitualTableScreen extends HandledScreen<RitualTableScreenHandler> {
     @Override
     protected void init() {
         super.init();
-        titleX = (backgroundWidth - textRenderer.getWidth(title)) / 2;
-        this.playerList = new RitualTablePlayerListWidget(this, this.client, this.width, this.height, getMargin(-79, getCenterY(), this.height), getMargin(-16, getCenterY(), this.height), 24);
+        titleX = getMargin(60, ((backgroundWidth - textRenderer.getWidth(title)) / 2), backgroundWidth);
+        this.playerList = new RitualTablePlayerListWidget(this, this.client, this.width, this.height, getMargin(-74, getCenterY(), this.height), getMargin(-16, getCenterY(), this.height), 24);
 
         this.resurrectButton = new ResurrectButtonWidget(getMargin(2, getCenterX(), this.height), getMargin(-30, getCenterY(), this.height),75,16,
                 new ButtonTextures(new Identifier(Revivals.MOD_ID, "container/ritual_table/button"),
